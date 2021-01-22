43b187a57725c61c5863e699b3e5287226571448
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2021-01-22 11:04:35 +0300

    Test

---
+++ figma12/Assets/closs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closs : MonoBehaviour
+{
+    public GameObject layout;
+
+    public void Close()
+    {
+        layout.gameObject.SetActive(false);
+    }
+}
